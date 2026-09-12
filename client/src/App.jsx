9bbc10787bbd210525a9d07acaa24505db64b8de
Punzalan, Dave M. S. <158295047+DavePunzalan16@users.noreply.github.com>
2026-09-12 13:56:46 +0800

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { GuestLayout, AuthLayout } from './pages/Layout.jsx'
+import AuthPage from './pages/AuthPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import BuilderPage from './pages/BuilderPage.jsx'
+import PreviewPage from './pages/PreviewPage.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      {/* Login Routes */}
+      <Route element={<GuestLayout />}>
+       <Route path='/login' element={<AuthPage mode="login"/>}/>
+       <Route path='/register' element={<AuthPage mode="register"/>}/>
+      </Route>
+
+      {/* Protected Routes */}
+      <Route element={<AuthLayout />}>
+       <Route path='/' element={<HomePage/>}/>
+       <Route path='/builder/:id' element={<BuilderPage />}/>
+       <Route path='/preview/:id' element={<PreviewPage />}/>
+      </Route>
+    </Routes>
   )
 }
 
