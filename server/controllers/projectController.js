cadcbdb0f1dde128ad2d36d04e0fcaa8ff9e731b
Punzalan, Dave M. S. <158295047+DavePunzalan16@users.noreply.github.com>
2026-09-26 02:17:39 +0800

    backend server completed

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,5 +1,6 @@
 import crypto from "crypto";
 import { Project } from "../models/Project.js";
+import { generateProject } from "../services/ai.js"
 
 function hashContent(content) {
     return crypto
@@ -71,6 +72,84 @@ export async function createProject(req, res) {
 // Background worker
 async function runBackgroundGeneration(projectId, prompt) {
     // TODO: AI generation logic
+    try{
+        console.log(`[Background AI] Starting generation for project ${projectId}`);
+        const result = await generataeProject(prompt, {
+            onPlan: async (plan) =>{
+                 console.log(`[Background AI] Starting generation for project ${projectId}. Planned ${plan.files.length} files.`);
+                 const fileList = plan.files.map((f)=>`-\' ${f.path}\': ${f.description}`).join("\n")
+
+                 await Project.findByIdAndUpdate(projecctId, {
+                    name: pla.projectName || "Generated Project",
+                    status: "generating",
+                    filesPlanned: plan.files,
+                    $push: {
+                        messages: {
+                            role: "assistant",
+                            content:`Planned website structure:\n${fileList}`,
+                            timestamp: new Date(),
+                        }
+                    }
+                 })
+            },
+            onFileStart: async (path)=>{
+                console.log(`[Background AI] Starting file ${path} for project ${projectId}`);
+                await Project.findByIdAndUpdate(projectId,{
+                    currentFile: path,
+                })
+            },
+             onFileComplete: async (path, code)=>{
+                console.log(`[Background AI] Finished file ${path} for project ${projectId}`);
+                
+                const project = await Project.findById(projectId);
+
+                if(project){
+                    project.files = project.files || {};
+                    project.files[path] = { content: code, hash:
+                    hashContent(code) };
+                    project.filesGenerated = [...(project.
+                    filesGenerated || []), path];
+                    project.messages.push({
+                        role: "assistant",
+                        content: `Created file "${path}"`,
+                        timestamp: new Date(),
+                    });
+                    project.currentFile = null;
+                    project.markModified("files");
+                    await project.save();
+                }
+            }
+        })
+
+        console.log(`[Background AI] Successfully generated Project ${projectId}`);
+
+        const project = await Project.findById(projectId);
+        if(project){
+            project.status = "completed";
+            project.version = 1;
+            if(result.description){
+                project.name = result.description;
+            }
+            project.messages.push({
+                role: "assistant",
+                content: `Website generation complete! You can view and edit the files.`,
+                timestamp: new Date(),
+            })
+            await project.save();
+        }
+    }catch(error){
+        console.error(`[Background AI] Fatal generation error for the project ${projectId}:`, err);
+
+        await Project.findByIdAndUpdate(projectId, {
+            status: "failed",
+            error: err.message,
+            $push: {
+                role: "assistant",
+                content: `❌ Generation failed: ${err.message}`,
+                timestamp: new Date(),
+            }
+        })
+    }
 }
 
 // GET /api/projects
--- server/routes/projectsRoutes.js
+++ server/routes/projectsRoutes.js
@@ -1,12 +1,13 @@
 import {Router } from "express";
 import { createProject, deleteProject, getProject, getPublicProject, listProjects, publishProject, updateProjectFiles } from "../controllers/projectController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { chat } from "../controllers/chatController.js";
 
 const projectRouter = Router();
 
-//Public route
+//Public route nganii
 projectRouter.get("public/:id", getPublicProject)
-//Protect all following routes
+//Protect all following routes at all cost muehehe
 projectRouter.use(authMiddleware)
 
 projectRouter.post("/", createProject)
@@ -15,5 +16,8 @@ projectRouter.get("/:id", getProject)
 projectRouter.delete("/:id", deleteProject)
 projectRouter.put("/:id/files", updateProjectFiles)
 projectRouter.post("/:id/publish", publishProject)
+
+//chat
+projectRouter.post("/:id/chat", chat)
 
 export default projectRouter;
