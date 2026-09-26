7e6b6236ccc06292b3ad3232546d4902cb827f98
Punzalan, Dave M. S. <158295047+DavePunzalan16@users.noreply.github.com>
2026-09-26 20:21:12 +0800

    Fix Vercel SPA preview and publish routes

--- client/src/pages/PreviewPage.jsx
+++ client/src/pages/PreviewPage.jsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Loading from '../components/Loading'
-import FullPagePreivew from '../components/FullPagePreivew'
-import { useAppContext } from '../context/AppContext'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "../components/Loading";
+import FullPagePreivew from "../components/FullPagePreivew";
+import { useAppContext } from "../context/AppContext";
 
 const PreviewPage = () => {
-  const { id } = useParams()
-  const {activeProject: project, loadingActiveProject: loading, loadProject} = useAppContext()
+    const { id } = useParams();
 
-  useEffect(() => {
-    if(id) {
-      loadProject(id)
+    const {
+        activeProject: project,
+        loadingActiveProject: loading,
+        loadProject,
+        user,
+        loadingUser,
+    } = useAppContext();
+
+    useEffect(() => {
+        if (!id || loadingUser || !user) return;
+
+        loadProject(id);
+    }, [id, loadingUser, user]);
+
+    if (loadingUser || loading || !project) {
+        return <Loading />;
     }
-  }, [id])
 
-  if (loading || !project) {
-    return <Loading />
-  }
+    return <FullPagePreivew files={project.files} />;
+};
 
-  return (
-    <FullPagePreivew files={project.files}/>
-  )
-}
-
-export default PreviewPage
+export default PreviewPage;
